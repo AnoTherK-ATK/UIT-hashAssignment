17889b60eeb2796d1af55bf758958105396fd3b9
AnoTherK-ATK <khanhvu212@gmail.com>
2023-03-24 11:01:43 +0700

    first commit

---
+++ code/generator.cpp
@@ -0,0 +1,40 @@
+﻿#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+set<__int128_t> se;
+
+string genKey(__int128_t n){
+    string s;
+    for(int i = 0; i < 100; ++i){
+        if((1 << i) & n){
+            s = '1' + s;
+        }else
+            s = '0' + s;
+    }
+    return s;
+}
+
+string genValue(){
+    string ans = "";
+    for(int i = 0; i < 50; ++i){
+        ans += (char)(rnd.next(65, 122));
+    }
+    return ans;
+}
+
+
+int main(int argc, char *argv[]){
+    registerGen(argc, argv, 1);
+    ios::sync_with_stdio(0); cin.tie(nullptr);
+    freopen("../test\\input.txt", "w", stdout);
+    int n = atoi(argv[1]);
+    cout << n << '\n';
+    while(n--){
+        __int128_t seed;
+        do{
+            seed = rnd.next(0ll, LLONG_MAX - 1) * rnd.next(0ll, LLONG_MAX - 1);
+        }while(se.find(seed) != se.end());
+        cout << genKey(seed) << ' ' << genValue() << '\n';
+    }
+}
---
+++ code/hash.cpp
@@ -0,0 +1,61 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+const int M = 1e7 + 7; //size of the hash table
+const int MOD = INT_MAX;
+
+struct Hash{
+    string hashTable[M]; //hash table size M
+
+    Hash(){
+        for(int i = 0; i < M; ++i)
+            hashTable[i] = "";
+    }
+
+    inline int getPow2(int pos, int N){
+        return N - pos + 1;
+    }
+
+    long long hashFunc(const string& binaryString){
+        int numKey[4] = {};
+        long long cnt = 0;
+        long long len = (long long)binaryString.size(); //lenght of key
+        for(int i = len - 1; i >= 0; --i){
+            if(binaryString[i] == '1'){//if the bit in position i is on
+                numKey[(getPow2(i, len) / 25)] ^= (1 << (getPow2(i, len) % 25));
+            }
+        }
+        long long hashNum = numKey[0];
+        for(int i = 1; i < 4; ++i){
+            hashNum = ((1ll * hashNum * 1ll * numKey[i])) % MOD;
+        }
+        return hashNum % M;
+    }
+
+    void insert(const string& key, const string& value){
+        long long index = hashFunc(key);
+        hashTable[index] = value;
+    }
+
+    string getValue(const string& key){
+        long long index = hashFunc(key);
+        return hashTable[index];
+    }
+} hashT;
+
+int n, collision;
+string binaryKey;
+string value;
+
+signed main(){
+    ios::sync_with_stdio(0); cin.tie(nullptr);
+    freopen("../test\\input.txt", "r", stdin);
+    cin >> n;
+    for(int i = 0; i < n; ++i){
+        cin >> binaryKey >> value;
+        if(hashT.getValue(binaryKey) != "")
+            ++collision;
+        hashT.insert(binaryKey, value);
+    }
+    cout << collision;
+}
